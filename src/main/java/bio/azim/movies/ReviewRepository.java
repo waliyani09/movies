7d420a5c5e3c0e79262d46804da68953750fbb2b
azimwaliyani <waliyani09@gmail.com>
2023-01-24 18:17:40 -0500

    Backend ready with review functionality and API tested using postman

---
+++ src/main/java/bio/azim/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package bio.azim.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
