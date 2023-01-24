41f337ce752e6664efa872dcea1ecd8abc4ae595
azimwaliyani <waliyani09@gmail.com>
2023-01-24 14:13:31 -0500

    Initial project setup

---
+++ src/test/java/bio/azim/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package bio.azim.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
