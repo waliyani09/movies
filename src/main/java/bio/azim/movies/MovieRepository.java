bdd85e4bb4b2d0385a2e4e671d6a4dfcf5a5f4cf
azimwaliyani <waliyani09@gmail.com>
2023-01-24 17:21:49 -0500

    changed key to imdbId instead of ObjectId

--- src/main/java/bio/azim/movies/MovieRepository.java
+++ src/main/java/bio/azim/movies/MovieRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
